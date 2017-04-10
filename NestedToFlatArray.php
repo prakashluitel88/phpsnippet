677f26f116d382cd2b01314bb0b2a87015a4dc76
unknown <prakash01@Fazil-PC.VACKER.NET>
2017-04-10 17:42:59 +0400

    Pushed Initial Program to github

---
+++ NestedToFlatArray.php
@@ -0,0 +1,16 @@
+<?php
+// Sample Integer Array
+$data_array = "[[1,2,[3]],4]";
+
+$data_json = json_decode(implode(" ", $data_array));
+integerArrayFlatten($data_json, $final_array);
+
+// Get Final array in flattened form
+echo '<pre>'; print_r( $final_array );
+
+/* Function to flatten Integer Array */
+function integerArrayFlatten($data_json, &$tempCopy) {
+    array_walk_recursive($data_json, function($temp)use(&$tempCopy) {
+    	$tempCopy[] = $temp;
+    });
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # phpsnippet
-This program will flatten an array of arbitrarily nested arrays of integers into a flat array of integers.
+
+How to flatten nested arrays  to flat array?
+* Program written in PHP
+* This program will flatten an array of arbitrarily nested arrays of integers into a flat array of integers.
+
+* Any questions/suggestions regarding snippet please send email to  prakashluitel88@gmail.com. 
+
+http://lprakash.com.np
