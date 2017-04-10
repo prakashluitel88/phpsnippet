3c551be0cdd06d22bebc3d7b4d7dd48df0bd2c35
unknown <prakash01@Fazil-PC.VACKER.NET>
2017-04-10 17:52:09 +0400

    Formatted code and edited array string

--- NestedToFlatArray.php
+++ NestedToFlatArray.php
@@ -1,16 +1,19 @@
 <?php
 // Sample Integer Array
-$data_array = "[[1,2,[3]],4]";
+$data_array = array("[[1,2,[3]],4]");
 
 $data_json = json_decode(implode(" ", $data_array));
 integerArrayFlatten($data_json, $final_array);
 
 // Get Final array in flattened form
-echo '<pre>'; print_r( $final_array );
+echo '<pre>';
+print_r($final_array);
 
 /* Function to flatten Integer Array */
-function integerArrayFlatten($data_json, &$tempCopy) {
-    array_walk_recursive($data_json, function($temp)use(&$tempCopy) {
-    	$tempCopy[] = $temp;
+function integerArrayFlatten($data_json, &$tempCopy)
+{
+    array_walk_recursive($data_json, function($temp) use (&$tempCopy)
+    {
+        $tempCopy[] = $temp;
     });
 }
--- NestedToFlatArrayFromLoop.php
+++ NestedToFlatArrayFromLoop.php
@@ -1,25 +1,28 @@
 <?php
 // Sample Array data
 $data_array = array(
-	"[[1,2,[3]],4]",
-	"[[1,2,[3]],4, [5]]",
+    "[[1,2,[3]],4]",
+    "[[1,2,[3]],4, [5]]"
 );
 
 // Recursive call of $data_array to get flatten array
-foreach ( $data_array as $arr ) {
-	$final_array = array();
-	$data_json = json_decode($arr);
-	
-	integerArrayFlatten($data_json, $final_array);
-	
-	// Get Final array in flattened form
-	echo '<pre>'; print_r( $final_array );
+foreach ($data_array as $arr) {
+    $final_array = array();
+    $data_json   = json_decode($arr);
+    
+    integerArrayFlatten($data_json, $final_array);
+    
+    // Get Final array in flattened form
+    echo '<pre>';
+    print_r($final_array);
 }
 
 
 /* Function to flatten Integer Array */
-function integerArrayFlatten($data_json, &$tempCopy) {
-    array_walk_recursive($data_json, function($temp)use(&$tempCopy) {
-    	$tempCopy[] = $temp;
+function integerArrayFlatten($data_json, &$tempCopy)
+{
+    array_walk_recursive($data_json, function($temp) use (&$tempCopy)
+    {
+        $tempCopy[] = $temp;
     });
 }
